a7e26547a68144d647ad348db9373da2518fbc7c
Steven <steven@threatsec.net>
2017-06-05 14:56:03 +0100

    Release 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+/*
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel
+		.breakpoints({
+			desktop: '(min-width: 737px)',
+			tablet: '(min-width: 737px) and (max-width: 1200px)',
+			mobile: '(max-width: 736px)'
+		})
+		.viewport({
+			breakpoints: {
+				tablet: {
+					width: 1080
+				}
+			}
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				alignment: 'center',
+				detach: false
+			});
+
+		// Off-Canvas Navigation.
+
+			// Title Bar.
+				$(
+					'<div id="titleBar">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+						'<span class="title">' + $('#logo').html() + '</span>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			//insert copyright year
+			var year = (new Date().getFullYear());
+			$('#copyrightYear').text(year);
+
+
+			//form post 
+
+			$("#contact-form").submit(function(e){
+				e.preventDefault();
+				
+				var name = $("#contact-name").val()
+				var email = $("#contact-email").val()
+				var message = $("#contact-message").val()		
+				
+				var formData = $("#contact-form").serialize()
+				
+				
+				// send ajax
+				$.ajax({
+					url: 'https://contact.threatsec.com/send', // url where to submit the request
+					type : "POST", // type of action POST || GET
+					dataType : 'json', // data type					
+					data: formData,					
+					success : function(result){						
+						if (result.msg === "ok"){
+							alert("Thank you, your email has been sent");
+						}
+						else {
+							alert("Error, unable to sent an email.  Please try again later");
+						}
+					},
+					error : function(xhr,status, error){
+						var errors =[]
+						var result = JSON.parse(xhr.responseText)
+						console.log(result)									
+						alert("Error: unable to send mail.\n\n* "+result.msg.replace(/,./g, "\n* "))
+					}
+				})
+			});
+
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#titleBar, #navPanel, #page-wrapper')
+						.css('transition', 'none');
+
+	});
+
+})(jQuery);
